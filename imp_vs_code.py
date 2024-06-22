6db608e5a5513322e7c45b8fe6de3ca7e5e666da
yuriy812 <sitecadilac@mail.ru>
2024-06-23 00:10:53 +0300

    lastSyncextensions

--- imp_vs_code.py
+++ imp_vs_code.py
@@ -19,3 +19,26 @@ with zipfile.ZipFile(os.path.join(output_folder, output_filename), 'w') as zipf:
             zipf.write(file_path, os.path.relpath(file_path, settings_folder))
 
 print('Настройки Visual Studio успешно экспортированы в ZIP-файл:', output_filename)
+
+import os
+import shutil
+import json
+
+# Папка с настройками расширений Visual Studio
+extensions_folder = r'c:\Users\yuriy\AppData\Roaming\Code\User\sync\extensions'
+
+# Папка, куда будем сохранять настройки
+output_folder = r'E:\proect python 3\extensions vs code'
+
+# Создаем папку для сохранения настроек, если она не существует
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
+
+# Копируем файлы настроек расширений в новую папку
+for foldername, subfolders, filenames in os.walk(extensions_folder):
+    for filename in filenames:
+        if filename.endswith('lastSyncextensions.json'):
+            file_path = os.path.join(foldername, filename)
+            shutil.copy2(file_path, output_folder)
+
+print('Настройки расширений Visual Studio успешно экспортированы в папку:', output_folder)
